76d8cc99395a2bf343e356fc5910ebbce9b79640
tuanan <pantoan288@gmail.com>
2024-12-03 22:38:20 +0700

    Adjust Dark div, Add link to Project1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,12 @@ const Footer = () => {
     return (
         <div>
             <footer>
-                <div className='mx-auto text-center'>
+                <div className='py-14 px-10 mx-10 text-center'>
                     <p>&copy; AP. All Rights Reserved 2024</p>
                     <div className="mt-2 space-x-4">
-                        <a href="#" className="hover:text-white">GitHub</a>
-                        <a href="#" className="hover:text-white">LinkedIn</a>
-                        <a href="#" className="hover:text-white">Portfolio</a>
+                        <a href="#" className="hover:text-cyan-500 dark:hover:text-white">GitHub</a>
+                        <a href="#" className="hover:text-cyan-500 dark:hover:text-white">LinkedIn</a>
+                        <a href="#" className="hover:text-cyan-500 dark:hover:text-white">Portfolio</a>
                     </div>
                 </div>
             </footer>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,14 +7,17 @@ const Projects = () => {
         <div className="">
           <h2 className="text-4xl font-bold text-center mb-10">Projects</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full ">
-            <div className="bg-amber-400/75 rounded-[30px] transition-transform duration-500 ease-in-out overflow-hidden hover:scale-105 hover:shadow-lg group"> {/*Project 1 as Card */}
-              <div className="text-right text-cyan-300 p-6 pr-12"> {/* Title of Project */}
+            <a href="https://2tuanan.github.io/uiux-portfolio/" 
+            className="bg-amber-400/90 rounded-[30px] transition-transform duration-500 
+            ease-in-out overflow-hidden hover:scale-105 hover:shadow-lg group"> {/*Project 1 as Card */}
+              <div className="text-right text-cyan-800 p-6 pr-12"> {/* Title of Project */}
                 <span>Website</span>
                 <h3 className="text-2xl">UI/UX Portfolio</h3>
               </div>
               <div> {/* Description of Project */}
                 <div className="">
-                  <figure className="pl-12 relative w-[800px] h-80 transition-transform duration-500 ease-in-out group-hover:-translate-y-3">
+                  <figure className="pl-12 relative w-[800px] h-80 transition-transform duration-500 
+                  ease-in-out group-hover:-translate-y-3">
                     <img
                       src="assets/images/ux-ui_portfolio.png"
                       alt=""
@@ -22,7 +25,7 @@ const Projects = () => {
                   </figure>
                 </div>
               </div>
-            </div>
+            </a>
             <div></div> {/*Project 2 as Card */}
           </div>
         </div>
