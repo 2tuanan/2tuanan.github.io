121bac615f676e7b8e0cfb9f66407be26927a8e4
tuanan <pantoan288@gmail.com>
2024-12-04 23:56:48 +0700

    Change to Paralax P1

--- src/components/Herobanner.jsx
+++ src/components/Herobanner.jsx
@@ -1,17 +1,22 @@
 import React from "react";
+import { Parallax } from "react-parallax";
 
 const Herobanner = () => {
   return (
-    <div>
-      <header className="w-screen h-80 md:h-96 z-1 relative flex flex-col justify-center items-center">
+    <Parallax 
+      bgImage="/assets/images/hero-banner.jpg"
+      strength={500}
+      className="w-screen h-80 md:h-96 z-0 flex justify-center items-center"
+    >
+      <div className="w-screen h-80 md:h-96 z-1 relative flex flex-col justify-center items-center">
         <h1 className="text-6xl md:text-8xl text-cyan-900 dark:text-cyan-300 font-bold tracking-wider animate-fade-in">
           Hi, I'm ...
         </h1>
         <p className="mt-4 text-xl md:text-2xl opacity-80 animate-slide-in">
           A Web Developer
         </p>
-      </header>
-    </div>
+      </div>
+    </Parallax>
   );
 };
 
