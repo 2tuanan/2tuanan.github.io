659366fef22f48a206c9d52cfb6e5139a4caf57f
tuanan <pantoan288@gmail.com>
2024-12-07 23:57:25 +0700

    Change to Paralax P3 (Adjust Herobanner)

--- src/components/Herobanner.jsx
+++ src/components/Herobanner.jsx
@@ -4,16 +4,21 @@ import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 const Herobanner = () => {
   return (
     <Parallax pages={2}>
-      <ParallaxLayer speed={0.35} style={{zIndex: -1}}>
+      <ParallaxLayer speed={0.35} style={{zIndex: -2}}>
         <img
         className="absolute bottom-0 w-screen"
         src="/assets/images/ForeMount.png" alt="" />
+      </ParallaxLayer>
+      <ParallaxLayer speed={0.38} style={{zIndex: -1}}>
+        <img
+        className="absolute bottom-0 w-screen"
+        src="/assets/images/BackTrees.png" alt="" />
       </ParallaxLayer>
       <ParallaxLayer speed={0.4}
       >
         <img
         className="absolute bottom-0 w-screen" 
-        src="/assets/images/Trees.png" alt="" />
+        src="/assets/images/ForeTrees.png" alt="" />
       </ParallaxLayer>
       <div className="w-screen h-80 md:h-96 z-1 relative flex flex-col justify-center items-center">
         <h1 className="text-6xl md:text-8xl text-cyan-900 dark:text-cyan-300 font-bold tracking-wider animate-fade-in">
